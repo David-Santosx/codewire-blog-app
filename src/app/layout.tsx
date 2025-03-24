58302984564246b7e3281942a47fc7db2ed99280
David-Santosx <willians.sant@proton.me>
2025-03-23 22:21:05 -0400

    refactor(adsense): move Google AdSense script directly into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,6 +106,22 @@ export default function RootLayout({
             >
               {children}
               <GoogleAdsense />
+              <script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1306875437034957"
+                crossOrigin="anonymous"
+              ></script>
+              <ins
+                className="adsbygoogle"
+                style={{ display: "block" }}
+                data-ad-client="ca-pub-1306875437034957"
+                data-ad-slot="5557634349"
+                data-ad-format="auto"
+                data-full-width-responsive="true"
+              ></ins>
+              <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+              </script>
             </ThemeProvider>
           </body>
         </html>
